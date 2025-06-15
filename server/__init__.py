7679ffc50ec358bafa880727d14f76c1c0eaef38
lenny <lenny.kimanthi@student.moringaschool.com>
2025-06-15 17:28:32 +0300

    edited app.py __init__.py for server hope im right mahnnn

--- server/__init__.py
+++ server/__init__.py
@@ -1,1 +1,12 @@
-from .app import create_app, db
+from flask import Flask
+from .config import Config
+from .models import db
+from .routes import api
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+    app.register_blueprint(api)
+
+    return app
--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
+from .models import Restaurant, Pizza, RestaurantPizza
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -10,11 +11,14 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///app.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
     db.init_app(app)
     migrate.init_app(app, db)
 
     @app.route('/')
     def index():
         return 'Pizza API Running'
-
+    
+    from .models import Restaurant, Pizza, RestaurantPizza
+    
     return app
