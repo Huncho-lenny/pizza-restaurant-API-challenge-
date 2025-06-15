4004b894652414909e798aa53b41c14cc726e280
lenny <lenny.kimanthi@student.moringaschool.com>
2025-06-15 16:43:52 +0300

    kuchanganyikiwa is reall

--- server/app.py
+++ server/app.py
@@ -1,16 +1,15 @@
 from flask import Flask
-from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from .config import config
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(config)
+    app.config.from_object("server.config")
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     return app
-
