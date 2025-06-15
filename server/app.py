a5f62d9c5941fb56bb58d7c46d7f1f298b26176b
lenny <lenny.kimanthi@student.moringaschool.com>
2025-06-15 17:20:25 +0300

    migrations and edited app and config .py i think

--- server/app.py
+++ server/app.py
@@ -1,15 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object("server.config")
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///app.db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     migrate.init_app(app, db)
+
+    @app.route('/')
+    def index():
+        return 'Pizza API Running'
 
     return app
