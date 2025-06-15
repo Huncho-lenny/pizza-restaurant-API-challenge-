4a86577838cdb6a0a732e0d2553a85d1855a84ab
lenny <lenny.kimanthi@student.moringaschool.com>
2025-06-15 14:48:17 +0300

    config.py

---
+++ server/config.py
@@ -0,0 +1,8 @@
+import os 
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+class config:
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'database.db')}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
