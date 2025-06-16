951a8db86bd94335584b26e9dd05c25a47fe6652
lenny <lenny.kimanthi@student.moringaschool.com>
2025-06-16 21:26:29 +0300

    extensions.py atp i dont know what to be scared of more the codility or submissions

--- server/app.py
+++ server/app.py
@@ -1,17 +1,16 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from .models import Restaurant, Pizza, RestaurantPizza
 import os
+from flask import Flask
+from flask_migrate import Migrate
+from server.extensions import db
 
-db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+    
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///app.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
+
     db.init_app(app)
     migrate.init_app(app, db)
 
